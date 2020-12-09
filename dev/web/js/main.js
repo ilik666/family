fbc0e31cc94cd6eeede6dae5f659dfe63469c306
Ametov Ildar <ametov.ildar@gmail.com>
2020-12-10 02:46:19 +0300

    new pages

--- dev/web/js/main.js
+++ dev/web/js/main.js
@@ -11,5 +11,81 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 		)
 	// 	})(this)
 	// };
+	
+	function createRipple(e) {
+		const button = e.target.closest('.g-btn--animate');
+		const ripple = document.querySelectorAll(".ripple");
+
+		if (ripple) ripple.forEach( el => el.remove() )
+
+		const circle = document.createElement("span");
+		const diameter = Math.max(button.clientWidth, button.clientHeight);
+		const radius = diameter / 2;
+	  
+		circle.style.width = circle.style.height = `${diameter}px`;
+		circle.style.left = `${e.offsetX - radius }px`;
+		circle.style.top = `${e.offsetY - radius }px`;
+		circle.classList.add("ripple");
+
+		button.appendChild(circle);
+	}
+	
+	const buttons = document.querySelectorAll('.g-btn--animate');
+	if(buttons.length) buttons.forEach( el => el.addEventListener('click', createRipple) )
+    
+    const studiosSlider = new Swiper('.our-studios-slider', {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        pagination: {
+            el: '.our-studios-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        } 
+    })
+    const consultantSlider = new Swiper('.consultants-slider', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        } 
+    })
+
+	
+    let scrollAnimationItems = document.querySelectorAll('[data-n]');
+    let startFlag = true
+    if (scrollAnimationItems.length) window.addEventListener('scroll', showAfterScroll);
+
+    const startedCallback = (elems, callback) => elems.forEach( el => callback(el))
+
+    function animateNumber(el, step = 1 ,interval = 50) {
+        let _max = el.dataset.n
+        let count = 0
+
+        if(_max > 100) step = 10
+        else if(_max > 1000) step = 15
+
+        let startTimer = setInterval(() => {
+            count = count + step
+            el.textContent = count
+
+            if(count >= _max) {
+                el.textContent = _max
+                clearInterval(startTimer)
+            }
+        }, interval / 2); 
+    }
+
+    function showAfterScroll() {
+        for (let i = 0; i < scrollAnimationItems.length; i++) {
+            if ((scrollAnimationItems[i].getBoundingClientRect().top < document.documentElement.clientHeight / 1.5 &&
+                scrollAnimationItems[i].getBoundingClientRect().top > 0)) {
+                    startFlag && startedCallback(scrollAnimationItems, animateNumber)
+                    startFlag = false
+            }
+        }
+    }
 
 })
