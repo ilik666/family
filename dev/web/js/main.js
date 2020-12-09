e1d6e7d1d02a2aa25a05abce2e47bd893f5a0367
i.ametov <ametov.ildar@gmail.com>
2020-12-09 14:15:09 +0300

    start

---
+++ dev/web/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// polyfill flat
+	// if (!Array.prototype.flat) Array.prototype.flat = function () {
+	// 	return (function f(arr) {
+	// 	return arr.reduce(
+	// 			(a, v) =>
+	// 	Array.isArray(v)
+	// 				? a.concat(f(v))
+	// 				: a.concat(  v )
+	// 			, []
+	// 		)
+	// 	})(this)
+	// };
+
+})
